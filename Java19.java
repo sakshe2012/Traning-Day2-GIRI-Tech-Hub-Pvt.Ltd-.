fef67289bbe2234d50e97757d3c406dc5e1830c5
SakshiKumbhar <sakshikumbhar@orchidengg.ac.in>
2026-05-19 21:53:23 +0530

    Update Java19.java

--- Java19.java
+++ Java19.java
@@ -1,3 +1,8 @@
+/*
+Q19. Create a Java program using switch to convert a given number (1-5) to its word equivalent (One, Two, ..., Five). If the number is not between 1 and 5, display “Invalid number”.
+Explanation:
+ Switch with cases 1 to 5; default to handle invalid numbers.
+*/
 import java.util.*;
 class Java19
 {
